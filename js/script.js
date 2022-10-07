96c63d515e25eeb814d37b3f7728a83dcdac3ece
DESKTOP-ESAV9NA\Asus <borno11050@hotmail.com>
2022-10-08 01:29:36 +0600

    launch

---
+++ js/script.js
@@ -0,0 +1,4 @@
+window.onload = () => {
+    document.getElementById('location').innerHTML = window.location.href;
+    document.getElementById('modification').innerHTML = document.lastModified;
+};
