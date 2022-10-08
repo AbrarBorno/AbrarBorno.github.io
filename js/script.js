210a5e31249f2ac880a058333cc2e27b8f183023
DESKTOP-ESAV9NA\Asus <borno11050@hotmail.com>
2022-10-08 21:24:32 +0600

    tidy css - ie doesn't support arrow functions >_<

--- js/script.js
+++ js/script.js
@@ -1,4 +1,10 @@
-window.onload = () => {
+/**
+ * Author: Abrar Mahmud
+ * ID: 18201147
+ * email: abrar.mahmud@g.bracu.ac.bd
+*/
+
+window.onload = function() {
     document.getElementById('location').innerHTML = window.location.href;
     document.getElementById('modification').innerHTML = document.lastModified;
 };
